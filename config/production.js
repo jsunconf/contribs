5d58e8336ba36e0286cfb35e239841e0cd1b2944
Robert Kowalski <rok@kowalski.gd>
2013-10-23 22:59:16 +0200

    heroku deployments

--- config/production.js
+++ config/production.js
@@ -27,55 +27,17 @@ var config = {
 , port: process.env.PORT || 4000
 , domain: 'http://localhost'
 , model: {
-    defaultAdapter: 'mongo'
-  }
-, db: {
-    mongo: {
-      username: null
-    , dbname: 'production'
-    , prefix: null
-    , password: null
-    , host: 'localhost'
-    , port: 27017
-    }
-  }
-
-/* // Using Postgres as the default, with only a Postgres DB
-, model: {
     defaultAdapter: 'postgres'
   }
 , db: {
     postgres: {
-      user: process.env.USER
-    , database: process.env.USER
-    , password: null
-    , host: null
-    , port: 5432
+      user: process.env.DB_USER
+    , database: process.env.DB_DATABASE
+    , password: process.env.DB_PW
+    , host: process.env.DB_HOST
+    , port: process.env.DB_PORT
     }
   }
-*/
-
-/* // Using Postgres as the default, with both Postgres and Riak
-, model: {
-    defaultAdapter: 'postgres'
-  }
-, db: {
-    postgres: {
-      user: process.env.USER
-    , database: process.env.USER
-    , password: null
-    , host: null
-    , port: 5432
-    }
-  , riak: {
-      protocol: 'http'
-    , host: 'localhost'
-    , port: 8098
-  }
-  }
-*/
 };
 
 module.exports = config;
-
-
